8b94e4bc7f4c2c3e0e9f52aa2565b70d1ed39894
Theleon <leonrej90@gmail.com>
2022-12-15 18:56:13 +0500

    Parallax HTML JS CSS

---
+++ main.js
@@ -0,0 +1,11 @@
+const handleScroll = (event) => {
+  const scrollPosition = event.target.scrollingElement.scrollTop;
+
+  const images = document.querySelectorAll("img");
+
+  images.forEach((element) => {
+    element.style.transform = `translate(0, ${scrollPosition / 10}px`;
+  });
+};
+
+window.addEventListener("scroll", handleScroll);
